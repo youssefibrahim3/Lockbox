448ee6bf56bc9dd1116cf504ad89ef71625a835b
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-09-20 10:01:23 -0600

    Realize member functions of Vault.cpp

--- lockbox/vault.cpp
+++ lockbox/vault.cpp
@@ -1,3 +1,24 @@
 #include "vault.h"
 
 Vault::Vault() {}
+
+void Vault::addAccount(Account account) {
+    accounts.push_back(account);
+}
+
+void Vault::addAccount(QString username, QString password, QString service) {
+    Account newAccount = Account(username, password, service);
+    accounts.push_back(newAccount);
+}
+
+void Vault::removeAccount(int index) {
+    accounts.erase(accounts.begin() + index);
+}
+
+Account& Vault::getAccount(int index) {
+    return accounts.at(index);
+}
+
+int Vault::getNumberOfAccounts()const {
+    return accounts.size();
+}
