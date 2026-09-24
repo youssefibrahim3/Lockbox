a7f463401232880245ec56bb9018a1e825d70b51
Youssef Ibrahim <youssefml.ibrahim@gmail.com>
2026-09-24 11:53:25 -0600

    Add Vault initialization into Main
    This is so that now, one Vault is initialized and passed between ui windows. I didn't do this before because I only had 1 window

--- lockbox/main.cpp
+++ lockbox/main.cpp
@@ -1,16 +1,19 @@
 #include "mainwindow.h"
 #include "logindialog.h"
-
+#include "vault.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
+    Vault vault;
+    vault.load("vault.json");
+
     LoginDialog l;
 
     if (l.exec() == QDialog::Accepted) {
-        MainWindow w;
+        MainWindow w(vault);
         w.show();
         return a.exec();
     }
